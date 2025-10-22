a998ca08544a96a46d808abc838c7e2137b042cb
26Vilela05 <victor.vilela011@gmail.com>
2025-10-22 08:24:41 -0300

    entity pronto

--- BACK/Próximos cruds/reviews.java
+++ BACK/Próximos cruds/reviews.java
@@ -14,9 +14,50 @@ public class reviews {
     private Long users_id;
     @Column(nullable = false)
     private Long store_id;
+    @Column(nullable = true)
     private int rating;
+    @Column(length = 500, nullable = true)
     private String comment;
+    @Column(nullable = false)
     private LocalDateTime created_at;
 
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getUsers_id() {
+        return users_id;
+    }
+    public void setUsers_id(Long users_id) {
+        this.users_id = users_id;
+    }
+    public Long getStore_id() {
+        return store_id;
+    }
+    public void setStore_id(Long store_id) {
+        this.store_id = store_id;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    public String getComment() {
+        return comment;
+    }
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+    
 
 }
--- BACK/Próximos cruds/stores.java
+++ BACK/Próximos cruds/stores.java
@@ -1,5 +1,86 @@
 package com.itb.inf2fm.innova.model.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "stores")
 public class stores {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String name;
+    @Column(nullable = true, length = 500)
+    private String description;
+    @Column(nullable = true, length = 255)
+    private String image_url;
+    @Column(nullable = true, length = 255)
+    private String website_url;
+    @Column(nullable = false)
+    private LocalDateTime created_at;
+    @Column(nullable = true)
+    private LocalDateTime updated_at;
+    @Column(nullable = false, length = 50)
+    private String status;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getWebsite_url() {
+        return website_url;
+    }
+    public void setWebsite_url(String website_url) {
+        this.website_url = website_url;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 
 }
